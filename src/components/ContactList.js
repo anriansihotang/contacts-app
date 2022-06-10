8eabc980cbc4006b3c85c55e8a993790dd23e9aa
Anrian Sihotang <anrishotang91@gmail.com>
2022-06-10 11:28:43 +0700

    menambahkan properti id , ondelete pada contact list

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ContactItem from "./ContactItem";
 
-const ContactList = ({ contacts }) => {
+const ContactList = ({ contacts,onDelete }) => {
 	return (
 		<div className="contact-list">
 			{
 				contacts.map((contact) =>{
-					return <ContactItem key={contact.id} {...contact} />
+					return <ContactItem 
+					key={contact.id}
+					id={contact.id}
+					onDelete={onDelete} 
+					{...contact} />
 				}) 
 			}
 		</div>
