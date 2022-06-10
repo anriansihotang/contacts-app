325cdb7e6d6f129f22fba016e7e2dd0735c23900
Anrian Sihotang <anrishotang91@gmail.com>
2022-06-10 12:58:48 +0700

    membuat contact app dari functional component menjadi class component

--- src/components/ContactApp.js
+++ src/components/ContactApp.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import ContactList from "./ContactList";
 import { getData } from "../utils/data";
 
 // * styling
 import '../styles/style.css';
-const ContactApp = () => {
-	const contacts = getData();
 
-	return (
-		<div className="contact-app">
-			<h1>Daftar Kontak</h1>
-			<ContactList contacts={contacts} />
-		</div>
-	);
+class ContactApp extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			contacts : getData()
+		}
+		this.onDeleteHandler = this.onDeleteHandler.bind(this);
+	}
+
+	onDeleteHandler(id) {
+		const contacts = this.state.contacts.filter(contact => contact.id !== id);
+		this.setState({ contacts });
+	}
+
+	render() {
+		return (
+			<div className="contact-app">
+				<h1>Daftar Kontak</h1>
+				<ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler}/>
+			</div>
+		);
+	}
 }
 
 export default ContactApp;
